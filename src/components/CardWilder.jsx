640a3127e24d65c7685b5262c9b2b0e713770d34
delbaze <56109313+delbaze@users.noreply.github.com>
2022-09-16 10:25:33 +0200

    image

--- src/components/CardWilder.jsx
+++ src/components/CardWilder.jsx
@@ -1,11 +1,20 @@
 import "./CardWilder.css";
+import logo from "./person.png";
 function CardWilder({ firstName }) {
   return (
     <div className="card">
       <div className="cardImage">
-        <img src="" />
+        <img src={logo} />
       </div>
-      <h4>{firstName}</h4>
+      <div className="cardTitle">
+        <h4>{firstName}</h4>
+      </div>
+      <div className="cardDescription">
+        description(à faire)
+      </div>
+      <div className="cardLanguages">
+        Langages
+      </div>
     </div>
   );
 }
